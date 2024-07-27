e9b7cee038650376fc400f25032170fca3aeec3e
9494Guzel <guzel.galiulina.94@inbox.ru>
2024-07-28 00:13:28 +0300

    Add files via upload

---
+++ module 3.2.py
@@ -0,0 +1,15 @@
+def send_email(massang, recipiend, sender='universiti.help@gmail.com'):
+    foi
+    i in send_email():
+    print(*i)
+    if (str(sender.endstwith('@', 'inbox', ' mail') and
+            str(send_email('com', 'ru', 'net'))
+            or str(recipiend.endstrwith('@', 'inbox', 'mail')) and
+            str(recipiend.endstrwith('com', 'ru', 'net')):
+    print("не возможно отправитьотправить письмо<sinder> на адрес <recipiend>")
+    elif recipiend == sender
+    print('нельзя отправить письмо самому себе ')
+    elif sinder == 'universiti.help@mail.com'
+    print('письмо успешно отправлено<sinder> на адрес<recipiend>')
+    else:
+        print('НЕ СТАНДАРТНЫЙ ОТПРАВИТЕЛЬ!письмо отправлено с адреса <sinder> на вдрес <recipiend>')
